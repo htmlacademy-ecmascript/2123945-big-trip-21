6260ad0e3f93abd97055488c2c5f11f460341daf
Alena Aleksenko <106320275+AleksenkoA@users.noreply.github.com>
2023-08-21 19:19:20 +0300

    настраивает проверку типизации, пишет main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import NewTripInfoView from "./view/trip-info-view.js";
+import NewFilterView from "./view/filter-view.js";
+import BoardPresenter
+
+import { render } from "./render";
+import NewEventListView from "./view/event-list-view.js";
+
+import NewPointEditView from "./view/point-edit-view.js";
+import NewPointView from "./view/point-view.js";
+import NewSortView from "./view/sort-view.js";
+
+
+
