0d55b7c3ab8a7f4e37521ead750a7036d645ef19
Alena Aleksenko <106320275+AleksenkoA@users.noreply.github.com>
2023-08-21 21:03:59 +0300

    вносит правки: удаляет ^, подключение bundle.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import NewTripInfoView from "./view/trip-info-view.js";
-import NewFilterView from "./view/filter-view.js";
-import BoardPresenter
+// import NewTripInfoView from "./view/trip-info-view.js";
+// import NewFilterView from "./view/filter-view.js";
+// import BoardPresenter
 
-import { render } from "./render";
-import NewEventListView from "./view/event-list-view.js";
+// import { render } from "./render";
+// import NewEventListView from "./view/event-list-view.js";
 
-import NewPointEditView from "./view/point-edit-view.js";
-import NewPointView from "./view/point-view.js";
-import NewSortView from "./view/sort-view.js";
-
-
-
+// import NewPointEditView from "./view/point-edit-view.js";
+// import NewPointView from "./view/point-view.js";
+// import NewSortView from "./view/sort-view.js";
