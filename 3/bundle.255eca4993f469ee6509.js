28cb175c8a5b362cf062db88d1ce367a19799935
keksobot <github+keksobot@htmlacademy.ru>
2023-08-22 11:20:37 +0000

    ✔️ Сборка #3

---
+++ 3/bundle.255eca4993f469ee6509.js
@@ -0,0 +1,31 @@
+(()=>{"use strict";class e extends HTMLElement{render(){this.innerHTML=this.createHtml()}createHtml(){return""}}const t=e;function i(e,...t){return e.reduce(((e,i,r)=>{const l=t[r-1];return void 0===l?e+i:Array.isArray(l)?e+l.join("")+i:e+l+i}))}customElements.define("brief-view",class extends t{constructor(){super(),this.classList.add("trip-info")}createHtml(){return i`
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">Amsterdam — Chamonix — Geneva</h1>
+        <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;20</p>
+      </div>
+
+      <p class="trip-info__cost">
+        Total: €&nbsp;<span class="trip-info__cost-value">1230</span>
+      </p>
+    `}});customElements.define("filter-view",class extends t{constructor(){super(),this.classList.add("trip-filters")}createHtml(){return i`
+      <div class="trip-filters__filter">
+        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked="">
+        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+        <label class="trip-filters__filter-label" for="filter-future">Future</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
+        <label class="trip-filters__filter-label" for="filter-present">Present</label>
+      </div>
+
+      <div class="trip-filters__filter">
+        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+        <label class="trip-filters__filter-label" for="filter-past">Past</label>
+      </div>
+    `}});const r=document.querySelector("brief-view"),l=document.querySelector("filter-view");r.render(),l.render()})();
+//# sourceMappingURL=bundle.255eca4993f469ee6509.js.map
