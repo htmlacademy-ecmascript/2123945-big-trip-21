7775dbd29664a4d5f8cc3384d2b49c6241174550
Alena Aleksenko <106320275+AleksenkoA@users.noreply.github.com>
2023-08-22 14:37:23 +0300

    добавляет AddButtonView

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
+import './views/add-button-view.js';
 
 /**
  * @type {import('./views/brief-view').default}
@@ -13,5 +14,12 @@ const BriefView = document.querySelector('brief-view');
 
 const FilterView = document.querySelector('filter-view');
 
+/**
+ * @type {import('./views/add-button-view').default}
+ */
+
+const AddButtonView = document.querySelector('add-button-view');
+
 BriefView.render();
 FilterView.render();
+AddButtonView.render();
