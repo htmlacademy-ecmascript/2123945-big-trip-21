975fac6c2896d1f30bd414008be621d5c77f1bb8
keksobot <github+keksobot@htmlacademy.ru>
2023-08-22 16:09:24 +0000

    ✔️ Сборка #3

--- 3/bundle.940f9c74a8f63531fd31.js
+++ 3/bundle.940f9c74a8f63531fd31.js
@@ -55,8 +55,58 @@
         <label class="trip-sort__btn" for="sort-offer">Offers</label>
       </div>
     `}});class i extends e{constructor(){super()}createHtml(){return r`
-
+      <div class="event">
+        ${this.createStartDateHtml()}
+        ${this.createTypeIconHtml()}
+        ${this.createDestinationHtml()}
+        ${this.createScheduleHtml()}
+        ${this.createPriceHtml()}
+        ${this.createOfferListHtml()}
+        ${this.createFavouriteButtonHtml()}
+        ${this.createOpenButtonHtml()}
+      </div>
+    `}createStartDateHtml(){return r`
+      <time class="event__date" datetime="2019-03-18">MAR 18</time>
+    `}createTypeIconHtml(){return r`
+      <div class="event__type">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+      </div>
+    `}createDestinationHtml(){return r`
+      <h3 class="event__title">Taxi Amsterdam</h3>
+    `}createScheduleHtml(){return r`
+      <div class="event__schedule">
+      <p class="event__time">
+        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+        —
+        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+      </p>
+      <p class="event__duration">30M</p>
+    </div>
+    `}createPriceHtml(){return r`
+      <p class="event__price">
+      €&nbsp;<span class="event__price-value">20</span>
+      </p>
+    `}createOfferListHtml(){return r`
+      <h4 class="visually-hidden">Offers:</h4>
+      <ul class="event__selected-offers">
+        <li class="event__offer">
+          <span class="event__offer-title">Order Uber</span>
+          +€&nbsp;
+          <span class="event__offer-price">20</span>
+        </li>
+      </ul>
+    `}createFavouriteButtonHtml(){return r`
+      <button class="event__favorite-btn event__favorite-btn--active" type="button">
+        <span class="visually-hidden">Add to favorite</span>
+        <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+          <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
+        </svg>
+      </button>
+    `}createOpenButtonHtml(){return r`
+      <button class="event__rollup-btn" type="button">
+        <span class="visually-hidden">Open event</span>
+      </button>
     `}}customElements.define("card-view",i);const s=i;class l extends e{constructor(){super()}createHtml(){return r`
 
-    `}}customElements.define("editor-view",l);const a=l;customElements.define("list-view",class extends e{constructor(){super(),this.classList.add("trip-list"),this.setAttribute("role","list")}render(){const t=new Array(4).fill().map(((t,e)=>{const r=0===e?new a:new s;return r.classList.add("trip-list__item"),r.setAttribute("role","listitem"),r.render(),r}));this.replaceChildren(...t)}});const n=document.querySelector("brief-view"),o=document.querySelector("filter-view"),d=document.querySelector("add-button-view"),p=document.querySelector("sort-view"),c=document.querySelector("list-view");n.render(),o.render(),d.render(),p.render(),c.render()})();
-//# sourceMappingURL=bundle.4e2c3062ef7a9834a72a.js.map
+    `}}customElements.define("editor-view",l);const a=l;customElements.define("list-view",class extends e{constructor(){super(),this.classList.add("trip-list"),this.setAttribute("role","list")}render(){const t=new Array(4).fill().map(((t,e)=>{const r=0===e?new a:new s;return r.classList.add("trip-list__item"),r.setAttribute("role","listitem"),r.render(),r}));this.replaceChildren(...t)}});const n=document.querySelector("brief-view"),c=document.querySelector("filter-view"),o=document.querySelector("add-button-view"),d=document.querySelector("sort-view"),p=document.querySelector("list-view");n.render(),c.render(),o.render(),d.render(),p.render()})();
+//# sourceMappingURL=bundle.940f9c74a8f63531fd31.js.map
