167d1895cba8822947c1e3f16daadd1831233045
Alena Aleksenko <106320275+AleksenkoA@users.noreply.github.com>
2023-08-24 14:03:25 +0300

    добавили AppModel для взаимодействия с данными

--- src/main.js
+++ src/main.js
@@ -3,39 +3,43 @@ import './views/filter-view.js';
 import './views/add-button-view.js';
 import './views/sort-view.js';
 import './views/list-view.js';
+import AppModel from './models/app-model.js';
+
+const appModel = new AppModel();
+
+appModel.ready().then(() => {
+  console.log(appModel.getPoints());
+  console.log(appModel.getDestinations());
+  console.log(appModel.getOfferGroups());
+});
 
 /**
  * @type {import('./views/brief-view').default}
  */
-
-const BriefView = document.querySelector('brief-view');
+const briefView = document.querySelector('brief-view');
 
 /**
  * @type {import('./views/filter-view').default}
  */
-
-const FilterView = document.querySelector('filter-view');
+const filterView = document.querySelector('filter-view');
 
 /**
  * @type {import('./views/add-button-view').default}
  */
-
-const AddButtonView = document.querySelector('add-button-view');
+const addButtonView = document.querySelector('add-button-view');
 
 /**
  * @type {import('./views/sort-view').default}
  */
-
-const SortView = document.querySelector('sort-view');
+const sortView = document.querySelector('sort-view');
 
 /**
  * @type {import('./views/list-view').default}
  */
+const listView = document.querySelector('list-view');
 
-const ListView = document.querySelector('list-view');
-
-BriefView.render();
-FilterView.render();
-AddButtonView.render();
-SortView.render();
-ListView.render();
+briefView.render();
+filterView.render();
+addButtonView.render();
+sortView.render();
+listView.render();
